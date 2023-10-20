fb329045432b3239e4bd174eec5fcd87d0312359
Celeste369 <celetesandoval0@gmail.com>
2023-10-20 11:55:26 -0300

    Agregando getFullYear() en getAll() y getOne()

--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,11 @@
 // Importacion del pool de conexión creado en database.js
 import { pool } from "./database.js";
 
+
+
 class LibroController{
-  // Accion de lectura de todos los registros de libros 
+  
+    // Accion de lectura de todos los registros de libros 
     async getAll(req, res) {
       try {
           const [result] = await pool.query('SELECT * FROM libros'); // Consulta por todos lo libros y guarga resultado en result
@@ -10,7 +13,10 @@ class LibroController{
           if (result.length === 0) { // Si el resultado de la consulta no contiene registros retorna messaje: "No se encontraron libros" 
               return res.status(404).json({ message: "No se encontraron libros" });
           }
-  
+          // Iterar sobre el resultado y modificar el campo añoPublicacion para mostrar solo el año
+        for (let libro of result) {
+            libro.añoPublicacion = new Date(libro.añoPublicacion).getFullYear();
+        }
           res.json(result); // Si el resultado de la consulta contiene registros los devuelve con formato json
       } catch (error) { // Lanza una excepcion por error interno del servidor
           console.error("Error al obtener todos los libros:", error);
@@ -28,8 +34,10 @@ class LibroController{
          res.status(404).json({ message: 'Libro no encontrado' });
          return;
          }
-  
-         res.json(result[0]); //Si el resultado de la consulta trae datos asociados, devuelve solo el primer libro, que coincide con el ID 
+         // Convertir la fecha completa del campo añoPublicacion a solo el año
+        result[0].añoPublicacion = new Date(result[0].añoPublicacion).getFullYear();
+
+         res.json(result[0]); 
       } catch(err) { // Lanza una excepciones por error que puede ocurrir en el servidor
           console.error(err);
           res.status(500).json({ message: 'Error interno del servidor' });
